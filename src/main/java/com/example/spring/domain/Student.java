126301a5b9114a45b4a8e173f26e157427529c2a
SeungWon Jung <jung946244@naver.com>
2023-09-21 14:13:24 +0900

    9.21 property

---
+++ src/main/java/com/example/spring/domain/Student.java
@@ -0,0 +1,27 @@
+package com.example.spring.domain;
+
+public class Student {
+    // email property (쓰기)
+    // information property (읽기, 쓰기)
+    // info property (x)
+    private String email;
+    private String info;
+
+//    public String getEmail() {
+//        return email;
+//    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInformation() {
+        return info;
+    }
+
+    public void setInformation(String info) {
+        this.info = info;
+    }
+
+
+}
