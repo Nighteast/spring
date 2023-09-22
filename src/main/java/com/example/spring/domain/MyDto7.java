61919d8c97eea168f9e94de557b044f461af8250
SeungWon Jung <jung946244@naver.com>
2023-09-22 11:36:01 +0900

    9.22

--- src/main/java/com/example/spring/domain/MyDto7.java
+++ src/main/java/com/example/spring/domain/MyDto7.java
@@ -3,7 +3,19 @@ package com.example.spring.domain;
 public class MyDto7 {
 
     public String getName() { // field랑 관련이 없다. 메서드에의해서 결정된다.
-        return "원붕";
+        return "JongLi";
+    }
+
+    public String getAddress() {
+        return "리월";
+    }
+
+    public String getEmail() {
+        return "abc@mihoyo.com";
+    }
+
+    public Integer getAge() {
+        return 6000;
     }
 
     @Override
