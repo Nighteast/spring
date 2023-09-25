cb13b8ee260df4057fc13c2d4b709362b79fed53
Nighteast <jung946244@naver.com>
2023-09-25 14:37:05 +0900

    9.25

--- src/main/java/com/example/spring/controller/Controller11.java
+++ src/main/java/com/example/spring/controller/Controller11.java
@@ -1,7 +1,13 @@
 package com.example.spring.controller;
 
+import com.example.spring.domain.MyDto9;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("main11")
@@ -21,7 +27,23 @@ public class Controller11 {
 
     }
 
-    @RequestMapping({"sub4", "sub5"})   //중괄호{}안에 쉼표로 여러개 사용 가능
+    @RequestMapping({"sub4", "sub5", "sub6"})   //중괄호{}안에 쉼표로 여러개 사용 가능
     public void method4() {
+    }
+
+    @RequestMapping("sub7")
+    public void method5(
+            @RequestParam(value ="show", defaultValue = "flase") Boolean show,
+                        Model model) {
+        if (show) {
+            List<MyDto9> list = new ArrayList<>();
+            list.add(new MyDto9(31L, "son", "hm", "football", "010"));
+            list.add(new MyDto9(32L, "lee", "aa", "paris", "011"));
+            list.add(new MyDto9(33L, "kim", "nn", "tokyo", "082"));
+            list.add(new MyDto9(34L, "jung", "ss", "seoul", "032"));
+            list.add(new MyDto9(35L, "yong", "hh", "busan", "014"));
+
+            model.addAttribute("moving", list);
+        }
     }
 }
---
+++ src/main/java/com/example/spring/controller/Controller12.java
@@ -0,0 +1,13 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main12")
+public class Controller12 {
+
+    @RequestMapping("sub1")
+    public void method1() {
+    }
+}
