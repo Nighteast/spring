350f6fed75957ba655a097f9250e3343f39e4ec5
Nighteast <jung946244@naver.com>
2023-10-10 15:28:01 +0900

    10.10 sql

--- src/main/java/com/example/spring/controller/Controller24.java
+++ src/main/java/com/example/spring/controller/Controller24.java
@@ -1,8 +1,11 @@
 package com.example.spring.controller;
 
+import com.example.spring.domain.MyDto17Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,14 +52,8 @@ public class Controller24 {
 
     // /main24/sub2 로 요청시
     // suppliers 테이블에 하나의 레코드 추가하는 메서드 요청
-    @GetMapping("sub2")
-    public void method2(@RequestParam(value = "1",defaultValue = "") String supplierName,
-                        @RequestParam(value = "2",defaultValue = "")String contactName,
-                        @RequestParam(value = "3",defaultValue = "")String address,
-                        @RequestParam(value = "4",defaultValue = "")String city,
-                        @RequestParam(value = "5",defaultValue = "")String postalCode,
-                        @RequestParam(value = "6",defaultValue = "")String country,
-                        @RequestParam(value = "7",defaultValue = "")String phone) throws SQLException {
+    @PostMapping("sub2")
+    public void method2(MyDto17Supplier supplier, Model model) throws SQLException {
         String sql = """
                 INSERT INTO suppliers (SupplierName, ContactName, Address, City, PostalCode, Country, Phone) 
                 VALUE (?,?,?,?,?,?,?)
@@ -65,27 +62,18 @@ public class Controller24 {
         Connection connection = dataSource.getConnection();
         PreparedStatement statement = connection.prepareStatement(sql);
 
-        // /main24/sub2?1=루미네&2=여행자&3=티바트&4=폰타인&5=444&6=몬드&7=01044444
-        statement.setString(1, supplierName);
-        statement.setString(2, contactName);
-        statement.setString(3, address);
-        statement.setString(4, city);
-        statement.setString(5, postalCode);
-        statement.setString(6, country);
-        statement.setString(7, phone);
-
-//        statement.setString(1, "손흥민");
-//        statement.setString(2, "ㅇㅇ");
-//        statement.setString(3, "토트넘");
-//        statement.setString(4, "런던");
-//        statement.setString(5, "123");
-//        statement.setString(6, "영국");
-//        statement.setString(7, "12314444");
-
-
-        List<Map<String, Object>> list = new ArrayList<>();
-
         try (connection; statement) {
+            // /main24/sub2?1=루미네&2=여행자&3=티바트&4=폰타인&5=444&6=몬드&7=01044444
+            statement.setString(1, supplier.getSupplierName());
+            statement.setString(2, supplier.getContactName());
+            statement.setString(3, supplier.getAddress());
+            statement.setString(4, supplier.getCity());
+            statement.setString(5, supplier.getPostalCode());
+            statement.setString(6, supplier.getCountry());
+            statement.setString(7, supplier.getPhone());
+
+            List<Map<String, Object>> list = new ArrayList<>();
+
             int count = statement.executeUpdate();
             if (count == 1) {
                 System.out.println("잘 입력됨");
@@ -94,6 +82,10 @@ public class Controller24 {
             }
 
         }
+
+    }
+    @GetMapping("sub3")
+    public void method3() {
 
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/main24/sub2.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2023-10-10
+  Time: 오후 2:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>공급자</h1>
+
+</body>
+</html>
