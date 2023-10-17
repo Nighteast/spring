2b3174b275f7c4b4633c967d0fa8f4395a8dc4e5
Nighteast <jung946244@naver.com>
2023-10-17 12:33:46 +0900

    10.17 DI, IOC

--- src/main/java/com/example/spring/MyApp7.java
+++ src/main/java/com/example/spring/MyApp7.java
@@ -1,16 +1,16 @@
 package com.example.spring;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class MyApp7 {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(MyApp7.class, args);
+        ApplicationContext context = SpringApplication.run(MyApp7.class, args);
         System.out.println(context.getBean("myClass12"));
         System.out.println(context.getBean("myClass11", MyClass11.class).getDep());
     }
