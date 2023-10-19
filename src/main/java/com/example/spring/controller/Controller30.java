918d219a1636850b302013aa7f3d019ef5564c59
Nighteast <jung946244@naver.com>
2023-10-19 10:52:29 +0900

    10.19 myBatis

--- src/main/java/com/example/spring/controller/Controller30.java
+++ src/main/java/com/example/spring/controller/Controller30.java
@@ -4,9 +4,11 @@ import com.example.spring.dao.MyDao4;
 import com.example.spring.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -90,24 +92,85 @@ public class Controller30 {
     public void method9() {
 
     }
+
     @PostMapping("sub10")
     public void method10(MyDto32 emp) {
         int row = dao.insert2(emp);
         System.out.println(row + "행이 입력됨");
     }
 
-
-    class Solution {
-        public int solution(int n) {
-            int answer = 0;
-
-            for (int i = 1; i <= n; i++) {
-                if (n % i == 0) {
-                    answer++;
-                }
-            }
-            return answer;
-        }
+    //  /main30/sub11?id=5
+    @GetMapping("sub11")
+    public void method11(Integer id) {
+        int rows = dao.delete1(id);
+        System.out.println(rows + "개 행이 지워짐");
     }
+
+    //  /main30/sub12?pid=3
+    //  3번 상품이 삭제되는 메서드 완성
+    @GetMapping("sub12")
+    public void method12(Integer pid) {
+        int rows = dao.delete2(pid);
+        System.out.println(rows + "개 행이 지워짐");
+    }
+
+    // /main30/sub13?id=2
+    @GetMapping("sub13")
+    public void method13(Integer id, Model model) {
+        //직원 조회
+        MyDto33Employee employee = dao.select8(id);
+
+        model.addAttribute("employee", employee);
+    }
+
+    @PostMapping("sub14")
+    public String method14(MyDto33Employee employee, RedirectAttributes rttr) {
+        //직원 수정
+        int rows = dao.update1(employee);
+
+        // 모델에 추가
+        if (rows == 1) {
+            rttr.addFlashAttribute("message", "직원 수정 완료");
+        } else {
+            rttr.addFlashAttribute("message", "정보가 수정되지 않았습니다.");
+        }
+
+        //쿼리스트링 추가
+        rttr.addAttribute("id", employee.getId());
+
+        return "redirect:/main30/sub13";
+    }
+
+    // GET /main30/sub15?id=3
+    // 3번 고객 조회 -> view로 포워딩
+    @GetMapping("sub15")
+    public void method15(Integer id, Model model) {
+        // 쿼리 파라미터 id, view 포워딩시 보여줄 model
+        MyDto34Customers customer = dao.select9(id);
+
+        model.addAttribute("customer", customer);   // view로 포워딩 한 후 꺼내 쓸 수 있도록
+        //  attributeName 으로 사용 한다.
+    }
+
+    // POST /main30/sub16
+    // 고객 정보 수정 -> /main30/sub15?id=3 로 redirect
+    @PostMapping("sub16")
+    public String method16(MyDto34Customers customer, RedirectAttributes rttr) {
+        //넘어온 정보들 customer로 받기 (자바 빈)
+
+        int rows = dao.update2(customer);
+
+        if (rows == 1) {
+            rttr.addFlashAttribute("message", "고객 정보가 수정되었습니다.");
+        } else {
+            rttr.addFlashAttribute("message", "고객 정보 수정에러");
+        }
+
+        //리턴하면서 쿼리에 붙여주기 ?id={getId}
+        rttr.addAttribute("id", customer.getId());
+
+        return "redirect:/main30/sub15";
+    }
+
 
 }
---
+++ src/main/java/com/example/spring/domain/MyDto34Customers.java
@@ -0,0 +1,14 @@
+package com.example.spring.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto34Customers {
+    private Integer id;
+    private String name;
+    private String contactName;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+}
