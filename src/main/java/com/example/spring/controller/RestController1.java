79995b59743a5daee5aa86efe9bd3500980fe77e
Nighteast <jung946244@naver.com>
2023-11-08 12:31:40 +0900

    @RestController 는 @Controller 와 @ResponseBody의 결합

--- src/main/java/com/example/spring/controller/RestController1.java
+++ src/main/java/com/example/spring/controller/RestController1.java
@@ -9,10 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+//@Controller
+//@ResponseBody   // 모든 메서드에 적용됨
+@RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 @RequestMapping("api/main1")
 public class RestController1 {
@@ -21,7 +24,6 @@ public class RestController1 {
 
     // http://localhost:8080/api/main1/sub1
     @GetMapping("sub1")
-    @ResponseBody
     public String method1() {
 
         return "customer name!!!";
@@ -31,21 +33,18 @@ public class RestController1 {
     // get 요청 시
     // 5번 고객의 customerName 응답
     @GetMapping("sub2")
-    @ResponseBody
     public String method2() {
 
         return dao.selectCustomerById(6);
     }
 
     @GetMapping("sub3")
-    @ResponseBody
     public MyDto34Customers method3() {
         return dao.getCustomer();
     }
 
     // /api/main1/sub4?id=10
     @GetMapping("sub4")
-    @ResponseBody
     public ResponseEntity<MyDto34Customers> method4(Integer id) {
 
         MyDto34Customers customer = dao.getCustomerById(id);
@@ -57,7 +56,6 @@ public class RestController1 {
     }
 
     @GetMapping("sub5")
-    @ResponseBody
     public ResponseEntity<MyDto33Employee> method5(Integer id) {
 
         MyDto33Employee employee = dao.getEmployeeById(id);
@@ -69,13 +67,11 @@ public class RestController1 {
     }
 
     @GetMapping("sub6")
-    @ResponseBody
     public List<Integer> method6() {
         return dao.getCustomerIdList();
     }
 
     @GetMapping("sub7")
-    @ResponseBody
     public List<Integer> method7() {
         return dao.getEmployeeIdList();
     }
